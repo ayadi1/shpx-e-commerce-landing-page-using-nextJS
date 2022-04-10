e23b8600f0b5a92298135629aa1e9b1d16e47e59
ayadi1 <ayadiossama44@gmail.com>
2022-04-10 17:46:31 +0000

    make footer and ads responsive

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,21 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta name="keywords" content="html,css,js,nextJS,reactJS" />
+        <meta name="description" content="SHPX | ecommerce landing page template by oussama ayadi" />
+        <meta name="author" content="oussama ayadi" />
+        <meta name="publisher" content="oussama ayadi" />
+        <meta name="copyright" content="oussama ayadi" />
+        <meta property="og:image" content="./assets/images/previewImage.png" />
+        <meta property="og:title" content="SHPX | ecommerce landing page" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:title" content="SHPX | ecommerce landing page " />
+        <meta
+          name="twitter:description"
+          content=" SHPX | ecommerce landing page. by oussama ayadi"
+        />
+        <meta name="twitter:image" content="./assets/images/previewImage.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <title>SHPX | ecommerce landing page</title>
       </Head>
       <Header />
